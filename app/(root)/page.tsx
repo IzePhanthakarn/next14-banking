ce24b7dcbf786888d81072d536e35ff80384d672
IzePhanthakarn <izephanthakarn@hotmail.com>
2024-05-05 23:56:43 +0700

    ✅ Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstname: "Phanthakarn" };
+  const loggedIn = { firstName: "Phanthakarn", lastName: "Khumphai", email: 'izephanthakarn@hotmail.com' };
 
   return (
     <section className="home">
@@ -11,7 +12,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstname || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and merge your account and transactions efficiently."
           />
 
@@ -21,7 +22,11 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50},{currentBalance: 500}]} />
     </section>
   );
 };
